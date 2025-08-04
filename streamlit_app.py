de7691d2f04708358c03844511f9895dd8e73d46
asivaram2 <asivaram@adobe.com>
2025-08-04 15:02:03 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,21 +4,21 @@ import requests
 # ---- Workfront API Config ----
 API_KEY = "4kocbwab1pws72grj41mhzicung7najj"
 PORTFOLIO_IDS = [
-  "62c75e2800b8e647336989ab99280223", //ATS Acquisitions 
-                            "62c75ed300b9233654df648eac740d68", //ATS Cloud Excellence
-                            "62c75ede00b9281b0e451c5038b36be2", //ATS Cloud Operations
-                            "62c75f2000b9468b026996cab2167547", //ATS Customer Experience Services
-                            "62c75f2f00b94ddef8f420a93626d39f", //ATS Data & Analytics 
-                            /*"62dedf37001f73ec09d059c9df63aa61", //ATS Recycle Bin*/
-                            "62c75f3c00b956ec671b80cb78a3ac1b", //ATS Digital Experience Services
-                            "62c75f4f00b95c19df68372ee87fd6a2", //ATS Emterprise Marketing
-                            "62c75f5c00b9675d20f08a5677a20412", //ATS Financial & Revenue Management
-                            "62e849e0001dcc7ab6d914774a13b0cf", //ATS Nestware
-                            "62c75f6d00b96a50b7d493c9cb5ae3a8", //ATS Office of the CIO
-                            "62c75fa900b9861c252cb07de7d099fe", //ATS Adobe Service Management
-                            "62c75f9800b97a8f221ce084c8d19d84", //ATS Sales Platform
-                            "62c75f5c00b9675d20f08a5677a20412", //ATS Shared Services Platform
-                            "67af7bb8000271dabb7cd574d194927c"  //ATS Transformation Management Office
+"62c75e2800b8e647336989ab99280223",  # ATS Acquisitions 
+    "62c75ed300b9233654df648eac740d68",  # ATS Cloud Excellence
+    "62c75ede00b9281b0e451c5038b36be2",  # ATS Cloud Operations
+    "62c75f2000b9468b026996cab2167547",  # ATS Customer Experience Services
+    "62c75f2f00b94ddef8f420a93626d39f",  # ATS Data & Analytics 
+    # "62dedf37001f73ec09d059c9df63aa61",  # ATS Recycle Bin (excluded)
+    "62c75f3c00b956ec671b80cb78a3ac1b",  # ATS Digital Experience Services
+    "62c75f4f00b95c19df68372ee87fd6a2",  # ATS Enterprise Marketing
+    "62c75f5c00b9675d20f08a5677a20412",  # ATS Financial & Revenue Management
+    "62e849e0001dcc7ab6d914774a13b0cf",  # ATS Nestware
+    "62c75f6d00b96a50b7d493c9cb5ae3a8",  # ATS Office of the CIO
+    "62c75fa900b9861c252cb07de7d099fe",  # ATS Adobe Service Management
+    "62c75f9800b97a8f221ce084c8d19d84",  # ATS Sales Platform
+    "62c75f5c00b9675d20f08a5677a20412",  # ATS Shared Services Platform
+    "67af7bb8000271dabb7cd574d194927c"   # ATS Transformation Management Office
                             
 ]
 BASE_URL = "https://adoberm.my.workfront.com/attask/api/v17.0/PROJ/search"
