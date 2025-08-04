3bdf36ce2db36752748f17c51edde5dbcf2a15a8
asivaram2 <asivaram@adobe.com>
2025-08-04 14:58:09 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,21 @@
+import streamlit as st
 import requests
 
-url = "https://adoberm.my.workfront.com/attask/api/v17.0/PROJ/search"
+# Call API on load or button click
+st.title("Select Workfront Project")
 
-params = {
-    "fields": 'name,status,entryDate,portfolioID,portfolio:name,program:name,DE:Effort Estimate',
-    "portfolioID": "62c75e2800b8e647336989ab99280223",  # example one portfolio ID
-    "apiKey": "4kocbwab1pws72grj41mhzicung7najj",
-    "$$LIMIT": "5"
-}
+if st.button("Load Projects"):
+    url = "https://adoberm.my.workfront.com/attask/api/v17.0/PROJ/search"
+    params = {
+        "fields": "name,status",
+        "portfolioID": "62c75e2800b8e647336989ab99280223",
+        "apiKey": "4kocbwab1pws72grj41mhzicung7najj",
+        "$$LIMIT": "20"
+    }
 
-response = requests.get(url, params=params)
-data = response.json()
+    r = requests.get(url, params=params)
+    projects = r.json()['data']
 
-# Example: print first project's name
-print(data['data'][0]['name'])
+    project_names = [proj["name"] for proj in projects]
+    selected = st.selectbox("Choose a project", project_names)
+    st.write(f"You selected: {selected}")
